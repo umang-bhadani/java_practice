5babc2afa47d345cb7ccc5ae2005fcfd2e31f8c0
Umang Kumar Bhadani <umangkumarbhadani35121@gmail.com>
2026-04-05 21:03:30 +0530

    Create smallest_largest.java

---
+++ array/smallest_largest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ArraysCC {
+    public static int getLargest(int numbers[]){
+        int largest=Integer.MIN_VALUE;
+        int smallest=Integer.MAX_VALUE;
+
+        for(int i=1;i<numbers.length;i++){
+            if(largest<numbers[i]){
+                largest=numbers[i];
+            }
+            if(smallest > numbers[i]){
+                smallest=numbers[i];
+            }
+        }
+        System.out.println("smallest value is :"+ smallest);
+        return largest;
+    }
+    public static void main(String args[]){
+        int numbers[]={1,2,6,3,5};
+        System.out.println("Largest value is :"+ getLargest(numbers));
+    }
+}
