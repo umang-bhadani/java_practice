677988f51b762139266fdd9392d62f0be2704692
Umang Kumar Bhadani <umangkumarbhadani35121@gmail.com>
2026-04-06 17:03:34 +0530

    Create sum_of_sub_array.java

---
+++ array/sum_of_sub_array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class SumofSubarrays {
+    public static void Max_sum_subarrays(int numbers[]){
+        int currSum=0;
+        int maxSum=Integer.MIN_VALUE;
+        for(int i=0;i<numbers.length;i++){
+            int start=i;
+            for(int j=i;j<numbers.length;j++){
+                int end=j;
+                currSum=0;
+                for(int k=start;k<end;k++){
+                    currSum +=numbers[k];
+                }
+                System.out.println(currSum);
+                if(maxSum<currSum){
+                    maxSum=currSum;
+                }
+            }
+        }
+        System.out.println("Max sum of subarrays:" + maxSum);
+    }
+    public static void main(String args[]){
+        int numbers[]={1,-2,6,-1,3};
+        Max_sum_subarrays(numbers);
+    }
+}
